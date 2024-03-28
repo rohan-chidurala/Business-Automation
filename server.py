beff115de2d3413a10d93628c596ff77d9c7245d
rohan chidurala <59586662+rohan-chidurala@users.noreply.github.com>
2024-03-27 17:00:29 -0700

    test3

--- server.py
+++ server.py
@@ -4,16 +4,16 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['GET'])
 def webhook():
-    data = request.get_json()
+    # Access query parameters
+    data = request.args.to_dict()  # Convert ImmutableMultiDict to dict
     
-    # This will return the entire JSON payload received from the request
+    # Return the query parameters as JSON
     return jsonify({'status': 'Success', 'received_data': data}), 200
 
 @app.route('/test', methods=['GET'])
 def test():
     # A simple endpoint to test if the server is running
     return jsonify({'message': 'Hello, World!'})
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
